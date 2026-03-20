5fdc80f082bd01d89f295900244a891fa6ae4659
soleyromit <soleyromit@gmail.com>
2026-03-20 18:39:12 -0400

    fix: base path + deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  base: "/rr-insights/",
-  build: { outDir: "dist" },
+  base: '/rr-insights/',
+  build: { outDir: 'dist' },
 });
